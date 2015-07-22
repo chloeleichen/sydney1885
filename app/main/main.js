ff6d8032687c93458815c84df58e19bcc94dc7da
Chloe <chloeleichen@gmail.com>
2015-07-22 23:16:57 +1000

    layout change

--- app/main/main.js
+++ app/main/main.js
@@ -1,28 +1,73 @@
-oldSydney.controller("mainController", 
+  oldSydney.controller("mainController", 
     ["$scope",
-     "$filter",
-     "$firebaseArray", 
-  function($scope, $filter, $firebaseArray) {
-    var ref = new Firebase('https://oldsydney.firebaseio.com');
-    $scope.imgs = $firebaseArray(ref);
-    //lazy load images
-    // $scope.$on('$viewContentLoaded', function load(){
-    //   echo.init({
-    //     offset: 0,
-    //     throttle: 250,
-    //     unload: false
-    //   });
-    //  });
+    "$filter",
+    "$firebaseArray", 
+    function($scope, $filter, $firebaseArray) {
+      var ref = new Firebase('https://oldsydney.firebaseio.com');
+      $scope.imgs = $firebaseArray(ref);
+      $scope.lClass = 'close';
+      //lazy load images
+      // $scope.$on('$viewContentLoaded', function load(){
+      //   echo.init({
+      //     offset: 0,
+      //     throttle: 250,
+      //     unload: false
+      //   });
+      //  });
 
-var marker, map;
-  $scope.$on('mapInitialized', function(evt, evtMap) {
-    map = evtMap;
-    marker = map.markers[0];
-  });
+    var map, infoWindow;
+    $scope.$on('mapInitialized', function(evt, evtMap) {
+      map = evtMap;
+    });
 
-  $scope.click = function(event) {
-    $scope.selectedMarker = this.title;
-    console.log(this);
+    var infoWindow = new google.maps.InfoWindow();
+    $scope.markers = [
+      {
+        title: "George Street",
+        position: [-33.870593, 151.206939]
+      },
+      {
+        title: "Fitzroy Dock",
+        position: [-33.848574, 151.172859]
+      },
+      {
+        title: "Hyde Park",
+        position: [-33.873138, 151.211276]
+      },
+      {
+        title: 'Oxford Street',
+        position: [-33.887304, 151.230648]
+      }
+    ];
+
+    $scope.markerClick = function(event) {
+      $scope.selectedMarker = this.title;
+      infoWindow.setContent(this.title);
+      infoWindow.open($scope.map, this);
+    }
+
+    var toggleClass = function(){
+      if($scope.lClass == 'open'){
+        $scope.lClass = 'close';
+      }else{
+        $scope.lClass = 'open';
+      }
+    }
+
+    $scope.photoClick = function(event){
+      $scope.lightbox = {
+        img: this.img.highreslink,
+        caption: this.img.caption
+      }
+      toggleClass(); 
+      var delay = setTimeout(function(){
+        echo.init();
+      }, 0);  
+    }
+
+    $scope.toggleClick = function(){
+      toggleClass();
+      $scope.lightbox = {};
+    }
   }
-}
 ]);
